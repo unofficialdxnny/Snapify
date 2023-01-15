07f44785d1f8c6408bf1c6231ade4f555d194f6d
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-01-15 02:49:09 +0000

    remaking it later :)
    this version is dead lol. Website has been updated

--- Snapify.py
+++ Snapify.py
@@ -62,7 +62,7 @@ options.add_argument(f'--profile-directory={data["path_to_chrome"]}')
 options.add_argument('--disable-blink-features=AutomationControlled')
 options.add_argument('--disable-blink-features=AutomationControlled')
 options.add_argument("--log-level=3")
-driver = webdriver.Chrome(options=options, service_args=["--verbose", "--log-path=D:\\qc1.log"]) ## Initialise the driver
+driver = webdriver.Chrome(options=options) ## Initialise the driver
 driver.get("https://accounts.snapchat.com/accounts/login") ## login to snapchat
 ## driver.maximize_window()
 wait = WebDriverWait(driver, 100)
@@ -96,56 +96,3 @@ wait = WebDriverWait(driver, 100)
 ## 
 ## login()
 
-if driver.current_url == 'https://accounts.snapchat.com/accounts/login':
-     print(Colorate.Color(Colors.yellow, 'Login to Snapchat'))
-
-
-if driver.current_url == 'https://accounts.snapchat.com/accounts/welcome':
-     print(Colorate.Color(Colors.yellow, 'Select "Snapchat Web"'))
-     sleep(2)
-     os.system('cls')
-     Write.Print(f"{banner}", Col.DynamicMIX((Col.white, Col.yellow)), interval=0)
-
-
-enable_camera = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "mBiMV")))
-enable_camera.click()
-while True:
-     if 'verify?' in driver.current_url:
-          print(Colorate.Color(Colors.yellow, 'Verify Login Attempt Via Your Phone.'))
-          sleep(2)
-          os.system('cls')
-          Write.Print(f"{banner}", Col.DynamicMIX((Col.white, Col.yellow)), interval=0)
-          ## options.headless = {headless}
-
-     
-     elif driver.current_url == 'https://web.snapchat.com/':
-          Write.Print(f"Accept Camera Perms Please...", Colors.yellow, interval=0)
-          os.system('cls')
-          Write.Print(f"{banner}", Col.DynamicMIX((Col.white, Col.yellow)), interval=0)
-          print(Colorate.Color(Colors.yellow, f'Snaps Sent : {snaps_sent}'))
-          sleep(2)
-          lines = data["lines_in_users_path"]
-          os.system('cls')
-          Write.Print(f"{banner}", Col.DynamicMIX((Col.white, Col.yellow)), interval=0)
-          sleep(2)
-          ## enable_camera = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "mBiMV")))
-          ## enable_camera.click()
-          sleep(2)
-          photo = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div[1]/div[2]/div/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div[1]/button[1]')))          
-          photo.click()
-          sleep(2)
-          send_to = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div[1]/div[2]/div/div/div/div/div[1]/div/div/div/div/div/button[2]')))
-          send_to.click()
-
-          with open(f'users_path.txt', 'r') as users:
-               lines = users.readlines()
-               for line in lines:
-                    user_select = wait.until(EC.presence_of_element_located((By.XPATH, f'{line}')))
-                    user_select.click()
-               
-               send = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="root"]/div[1]/div[2]/div/div/div/div/div[1]/div/div/div/div/div[1]/form/div[4]/button')))
-               send.click()
-               snaps_sent = snaps_sent+data["lines_in_users_path"]
-          
-
-               driver.minimize_window
