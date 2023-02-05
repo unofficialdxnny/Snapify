f5da2913593083b2e9fc28adaa72853a01ebef05
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-02-05 02:48:38 +0000

    Made A Frame

--- v4/main.js
+++ v4/main.js
@@ -2,8 +2,15 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
     const win = new BrowserWindow({
-      width: 800,
-      height: 600
+      width: 1200,
+      height: 700,
+      autoHideMenuBar: true,
+      icon:'./Assets/Images/favicon.png',
+      maxWidth: 1200,
+      maxHeight: 700,
+      fullscreenable: false,
+      
+      
     })
   
     win.loadFile('index.html')
