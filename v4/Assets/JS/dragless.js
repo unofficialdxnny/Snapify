c680139b14d3c6da7a5bd8ed72fa3a7cfc1b3d03
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-02-15 16:25:18 +0000

    dont have plans for v4 anymore

--- v4/Assets/JS/dragless.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll("*").forEach((elem) => {
-    elem.setAttribute('draggable', false)
-    elem.addEventListener('dragstart', (event) => {
-        event.preventDefault()
-    })
-})
