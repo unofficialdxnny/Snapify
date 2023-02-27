0f6775b57ffb19cd0c21ab0811dfad8082b87634
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2023-02-27 16:59:12 +0000

    Update Snapify.py

--- v4/Snapify.py
+++ v4/Snapify.py
@@ -21,19 +21,7 @@ data = json.load(f)
 
 
 
-options = Options()
-options.page_load_strategy = 'eager'
-## options.headless = headless
-options.add_argument(f"--user-data-dir={data['path_to_user_data']}")
-options.add_argument(f'--profile-directory={data["path_to_chrome"]}')
-options.add_argument('--disable-blink-features=AutomationControlled')
-options.add_argument('--disable-blink-features=AutomationControlled')
-options.add_argument("--log-level=OFF")
-options.add_experimental_option("detach", True)
-options.add_experimental_option('excludeSwitches', ['enable-logging'])
-driver = webdriver.Chrome(options=options) ## Initialise the driver
-## driver.maximize_window()
-wait = WebDriverWait(driver, 100)
+
 
 
 ## banner i just nicked from @Quessts
@@ -50,7 +38,7 @@ banner = '''
           /                   @@           #1 Free Snapchat booster.
          @@                    @%          Developed by @unofficialdxnny
        @@                       &@#        
-  %@@@*                            @@@@*   V4 | need help? Join Server : https://discord.gg/8WyFZF3kqn
+  %@@@*                            @@@@*   V4 | Help : snapify help
   &@@@@(                          @@@@@
        @@@@@@@             #@@@@@@@
                @@@#@@(##@@(
@@ -67,13 +55,17 @@ print('')
 
 while True:
     Snapify_input = Write.Input(f"Snapify> ", Col.DynamicMIX((Col.white, Col.yellow)), interval=0).lower()
-    Snapify_input = Snapify_input.split()
+
+
 
     if Snapify_input == 'clear':
         os.system('cls')
         Write.Print(f"{banner}", Col.DynamicMIX((Col.white, Col.yellow)), interval=0)
 
     elif Snapify_input == 'snapify':
+        os.system('cls')
+        Write.Print(f"{banner}", Col.DynamicMIX((Col.white, Col.yellow)) 
+, interval=0)
         Write.Print(f"Snapify Details...", Col.DynamicMIX((Col.white, Col.yellow)), interval=0)
         print('')
         print_json(data=data)
@@ -81,31 +73,63 @@ while True:
 
 
     elif Snapify_input == 'snapify help':
+        os.system('cls')
+        Write.Print(f"{banner}", Col.DynamicMIX((Col.white, Col.yellow)) 
+, interval=0)
         Write.Print(f"--------------------------------------------------------------------------\n", Col.DynamicMIX((Col.green, Col.green)), interval=0)
-        Write.Print(f"{driver.capabilities['browserVersion']}\n", Col.DynamicMIX((Col.green, Col.green)), interval=0)
+        ## Write.Print(f"{driver.capabilities['browserVersion']}\n", Col.DynamicMIX((Col.green, Col.green)), interval=0)
+        Write.Print("snapify : Current settings being used for Snapify\n", Col.DynamicMIX((Col.green, Col.green)), interval=0)
+        Write.Print("snapify help : List of commands\n", Col.DynamicMIX((Col.white, Col.green)), interval=0)
+        Write.Print("snapify score boost : Boost snapscore\n", Col.DynamicMIX((Col.white, Col.green)), interval=0)
 
+        Write.Print(f"--------------------------------------------------------------------------\n", Col.DynamicMIX((Col.green, Col.green)), interval=0)
 
     elif Snapify_input == 'snapify score boost':
-        driver.get("https://web.snapchat.com") ## login to snapchat
-        input('Press Enter When Page Ready...')
 
-        driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[2]/div/div/div/div/div[1]/div/div/div/div').click()
-        sleep(5)
-        driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[2]/div/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div[1]/button[1]').click()
-        sleep(5)
-        driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[2]/div/div/div/div/div[1]/div/div/div/div/div/button[2]').click()
-        with open('users_path.txt', 'r+') as snap_path:
-            lines = snap_path.readlines()
-            for line in lines:
-                driver.find_element(By.XPATH, f'{line}').click()
-          
-        driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[2]/div/div/div/div/div[1]/div/div/div/div/div[1]/form/div[3]/button').click()
+        x = 0
+
+        while True
+            os.system('cls')
+            Write.Print(f"{banner}", Col.DynamicMIX((Col.white, Col.yellow)) 
+, interval=0)
+            options = Options()
+            options.page_load_strategy = 'eager'
+            ## options.headless = headless
+            options.add_argument(f"--user-data-dir={data['path_to_user_data']}")
+            options.add_argument(f'--profile-directory={data["path_to_chrome"]}')
+            options.add_argument('--disable-blink-features=AutomationControlled')
+            options.add_argument('--disable-blink-features=AutomationControlled')
+            options.add_argument("--log-level=OFF")
+            options.add_experimental_option("detach", True)
+            options.add_experimental_option('excludeSwitches', ['enable-logging'])
+            driver = webdriver.Chrome(options=options) ## Initialise the driver
+            ## driver.maximize_window()
+            wait = WebDriverWait(driver, 100)
+            driver.get("https://web.snapchat.com") ## login to snapchat
+            input('Press Enter When Page Ready...')
+    
+            driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[2]/div/div/div/div/div[1]/div/div/div/div').click()
+            sleep(5)
+            driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[2]/div/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div[1]/button[1]').click()
+            sleep(5)
+            driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[2]/div/div/div/div/div[1]/div/div/div/div/div/button[2]').click()
+            with open('users_path.txt', 'r+') as snap_path:
+                lines = snap_path.readlines()
+                for line in lines:
+                    driver.find_element(By.XPATH, f'{line}').click()
+              
+            driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[2]/div/div/div/div/div[1]/div/div/div/div/div[1]/form/div[3]/button').click()
 
 
 
 
 
     elif Snapify_input == 'snapify help':
+
+        os.system('cls')
+        Write.Print(f"{banner}", Col.DynamicMIX((Col.white, Col.yellow)) 
+, interval=0)
+        
         Write.Print(f"--------------------------------------------------------------------------\n", Col.DynamicMIX((Col.green, Col.green)), interval=0)
         Write.Print(f"Snapify Help\n", Col.DynamicMIX((Col.white, Col.yellow)), interval=0)
         Write.Print(f"clear : Reset the terminal to its default look\n", Col.DynamicMIX((Col.white, Col.yellow)), interval=0)
@@ -115,12 +139,3 @@ while True:
 
         Write.Print(f"--------------------------------------------------------------------------\n", Col.DynamicMIX((Col.green, Col.green)), interval=0)
 
-
-            
-        
-
-
-
-
-
-        
