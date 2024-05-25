ca2b576016e4f07249c8d1f330d96a4f0160bc1f
Danial Ahmed <82535503+unofficialdxnny@users.noreply.github.com>
2024-05-25 19:28:27 +0100

    removed menubarr

--- v5/src/main/main.js
+++ v5/src/main/main.js
@@ -1,15 +1,20 @@
 // src/main/main.js
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 const path = require('path');
 
-function createWindow () {
+function createWindow() {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
     }
   });
+
+  // Remove the menu bar
+  Menu.setApplicationMenu(null);
 
   mainWindow.loadFile(path.join(__dirname, '../../public/index.html'));
 }
